049981689b4d1c1d59c6e21fbd8d513ee74bb201
DenysLes <157131711+DenysLes@users.noreply.github.com>
2025-06-16 20:47:51 +0300

    Add folders and add files jsx , css . Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,41 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+// import Statistics from '../components/Statistics/Statistics';
+// import FeedbackOptions from '../components/FeedbackOptions/FeedbackOptions';
+// import Section from '../components/Section/Section';
+// import Notification from '../components/Notification/Notification';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+  counPositiveFeedbackPercentage = () => {
+    const total = this.countTotalFeedback();
+    const { good } = this.state;
+
+    if (total === 0) {
+      return 0;
+    }
+    return Math.round((good / total) * 100);
+  };
+
+  onLeaveFeedback = event => {
+    const buttonName = event.target.name;
+    this.setState(prev => ({ [buttonName]: prev[buttonName] + 1 }));
+  };
+  render() {
+    // const total = this.countTotalFeedback();
+    // const positivePercentage = this.counPositiveFeedbackPercentage();
+    // const totalFeedback = this.countTotalFeedback();
+
+    return <div>react</div>;
+  }
+}
+
+export default App;
---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,4 @@
+// import css from './FeedbackOptions.module.css';
+// import PropTypes from 'prop-types'
+
+// export default FeedbackOptions
